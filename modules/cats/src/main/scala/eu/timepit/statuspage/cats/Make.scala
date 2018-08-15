424cfdbfca8bec128a9be831c4c45d4809f48375
Frank S. Thomas <frank@timepit.eu>
2018-08-15 19:56:54 +0200

    Move Make into cats package

--- modules/cats/src/main/scala/eu/timepit/statuspage/cats/Make.scala
+++ modules/cats/src/main/scala/eu/timepit/statuspage/cats/Make.scala
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package eu.timepit.statuspage
+package eu.timepit.statuspage.cats
 
 import _root_.cats.ApplicativeError
 import _root_.cats.instances.list._
@@ -25,31 +25,27 @@ import eu.timepit.statuspage.core.Item.{Entry, Group}
 import eu.timepit.statuspage.core.Result.{Error, Info, Ok}
 import eu.timepit.statuspage.core.{overallOf, Item, Result, Root}
 
-object cats {
+final class Make[F[_], E](val showError: E => String)(implicit F: ApplicativeError[F, E]) {
 
-  final class Make[F[_], E](val showError: E => String)(implicit F: ApplicativeError[F, E]) {
+  def root(items: F[Item]*): F[Root] =
+    items.toList.sequence.map(xs => Root(xs, overallOf(xs)))
 
-    def root(items: F[Item]*): F[Root] =
-      items.toList.sequence.map(xs => Root(xs, overallOf(xs)))
+  def group(name: String, items: F[Item]*): F[Item] =
+    items.toList.sequence.map(xs => Group(name, xs, overallOf(xs)))
 
-    def group(name: String, items: F[Item]*): F[Item] =
-      items.toList.sequence.map(xs => Group(name, xs, overallOf(xs)))
+  def entry[A](name: String, fa: F[A])(f: A => Result): F[Item] =
+    fa.attempt.map(ea => Entry(name, resultFromEither(ea)(f)))
 
-    def entry[A](name: String, fa: F[A])(f: A => Result): F[Item] =
-      fa.attempt.map(ea => Entry(name, resultFromEither(ea)(f)))
+  def entryOk[A](name: String, fa: F[A]): F[Item] =
+    entry(name, fa)(_ => Ok)
 
-    def entryOk[A](name: String, fa: F[A]): F[Item] =
-      entry(name, fa)(_ => Ok)
+  def entryInfo(name: String, fmsg: F[String]): F[Item] =
+    entry(name, fmsg)(msg => Info(msg))
 
-    def entryInfo(name: String, fmsg: F[String]): F[Item] =
-      entry(name, fmsg)(msg => Info(msg))
+  def resultFromEither[A](either: Either[E, A])(f: A => Result): Result =
+    either.fold(resultFromError, f)
 
-    def resultFromEither[A](either: Either[E, A])(f: A => Result): Result =
-      either.fold(resultFromError, f)
-
-    def resultFromError(e: E): Result =
-      Error.withMessage(showError(e))
-
-  }
+  def resultFromError(e: E): Result =
+    Error.withMessage(showError(e))
 
 }
