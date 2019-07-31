e6491482e413f32a88da81f3128f5ffe2a42b455
Scala Steward <me@scala-steward.org>
2019-08-01 00:39:57 +0200

    Update cats-effect to 1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,6 @@ import sbt.stringToOrganization
 
 object Dependencies {
   final val catsCore = "org.typelevel" %% "cats-core" % "1.6.1"
-  final val catsEffect = "org.typelevel" %% "cats-effect" % "1.3.1"
+  final val catsEffect = "org.typelevel" %% "cats-effect" % "1.4.0"
   final val scalatest = "org.scalatest" %% "scalatest" % "3.0.8"
 }
