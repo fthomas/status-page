319c70b707805caf306a4c00432a83a342c2feb5
Scala Steward <me@scala-steward.org>
2021-02-22 02:16:48 +0100

    Update cats-core to 2.4.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt.stringToOrganization
 
 object Dependencies {
-  final val catsCore = "org.typelevel" %% "cats-core" % "2.3.1"
+  final val catsCore = "org.typelevel" %% "cats-core" % "2.4.2"
   final val scalatest = "org.scalatest" %% "scalatest" % "3.2.5"
 }
