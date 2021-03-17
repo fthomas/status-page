c142c7601cbdc766532d1deb2d056d5394161493
Frank S. Thomas <frank@timepit.eu>
2021-03-17 11:15:11 +0100

    Sequence items with a Parallel instance

--- modules/cats/src/main/scala/eu/timepit/statuspage/cats/Make.scala
+++ modules/cats/src/main/scala/eu/timepit/statuspage/cats/Make.scala
@@ -16,21 +16,21 @@
 
 package eu.timepit.statuspage.cats
 
-import _root_.cats.ApplicativeError
-import _root_.cats.instances.list._
-import _root_.cats.syntax.applicativeError._
-import _root_.cats.syntax.functor._
-import _root_.cats.syntax.traverse._
+import _root_.cats.syntax.all._
+import _root_.cats.{MonadError, Parallel}
 import eu.timepit.statuspage.core.Item.{Entry, Group}
 import eu.timepit.statuspage.core.Result.{Error, Info, Ok}
 import eu.timepit.statuspage.core.{overallOf, Item, Result, Root}
 
-final class Make[F[_], E](val showError: E => String)(implicit F: ApplicativeError[F, E]) {
+final class Make[F[_], E](val showError: E => String)(implicit
+    parallel: Parallel[F],
+    F: MonadError[F, E]
+) {
   def root(items: F[Item]*): F[Root] =
-    items.toList.sequence.map(xs => Root(xs, overallOf(xs)))
+    items.toList.parSequence.map(xs => Root(xs, overallOf(xs)))
 
   def group(name: String, items: F[Item]*): F[Item] =
-    items.toList.sequence.map(xs => Group(name, xs, overallOf(xs)))
+    items.toList.parSequence.map(xs => Group(name, xs, overallOf(xs)))
 
   def entry[A](name: String, fa: F[A])(f: A => Result): F[Item] =
     fa.attempt.map(ea => Entry(name, resultFromEither(ea)(f)))
