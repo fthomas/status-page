bd526ef91b3cba0d7f36496f8c49535672740622
Scala Steward <me@scala-steward.org>
2022-01-24 13:03:22 +0100

    Update scalatest to 3.2.11

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt.stringToOrganization
 
 object Dependencies {
   final val catsCore = "org.typelevel" %% "cats-core" % "2.7.0"
-  final val scalatest = "org.scalatest" %% "scalatest" % "3.2.10"
+  final val scalatest = "org.scalatest" %% "scalatest" % "3.2.11"
 }
