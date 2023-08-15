4e6cc92a517643d3da80a25d4d88f240cb34461f
Scala Steward <scala_steward@virtuslab.com>
2023-08-15 15:44:40 +0000

    Update cats-core to 2.10.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,6 +1,6 @@
 import sbt.stringToOrganization
 
 object Dependencies {
-  final val catsCore = "org.typelevel" %% "cats-core" % "2.9.0"
+  final val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
   final val munit = "org.scalameta" %% "munit" % "0.7.29"
 }
