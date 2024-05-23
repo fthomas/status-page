6b1fcc81ab24768a9461fa1f746e3975b6a13266
Scala Steward <scala_steward@virtuslab.com>
2024-05-23 16:03:22 +0000

    Update munit to 1.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt.stringToOrganization
 
 object Dependencies {
   final val catsCore = "org.typelevel" %% "cats-core" % "2.10.0"
-  final val munit = "org.scalameta" %% "munit" % "0.7.29"
+  final val munit = "org.scalameta" %% "munit" % "1.0.0"
 }
